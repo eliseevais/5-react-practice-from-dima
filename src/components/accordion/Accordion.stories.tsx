4e02294844e36a045339fe1ef070b7c69dc8b974
Ирина Елисеева <irina.eliseeva1991@gmail.com>
2023-12-23 16:57:07 +0300

    first commit

---
+++ src/components/accordion/Accordion.stories.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+type AccordionPropsType = {
+  titleValue: string;
+  collapsed: boolean;
+  onChange: () => void;
+}
+export const Accordion = (props: AccordionPropsType) => {
+  console.log('Accordion is rendering');
+
+  return (
+    <div>
+      <AccordionTitle title={props.titleValue}
+                      onChange={props.onChange}
+      />
+      { !props.collapsed && <AccordionBody />}
+    </div>
+  );
+};
+
+type AccordionTitlePropsType = {
+  title: string;
+  onChange: () => void;
+};
+
+function AccordionTitle(props: AccordionTitlePropsType) {
+  console.log('AccordionTitle is rendering');
+  return (
+    <h3 onClick={props.onChange}>
+      ---{props.title}---
+    </h3>
+  )
+};
+
+function AccordionBody() {
+  console.log('AccordionBody is rendering');
+  return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  )
+}
+
